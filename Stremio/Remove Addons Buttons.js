9fcf1e0877beedf1835da0ac2da8977040a51f69
BreezeSpark <site.gethub@outlook.com>
2024-08-09 08:28:03 -0500

    Create Remove Addons Buttons.js

---
+++ Stremio/Remove Addons Buttons.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Stremio - Remove Addons Buttons
+// @namespace   https://github.com/BreezeSpark
+// @match       https://web.stremio.com/*
+// @version     1.0
+// @author      BreezeSpark
+// @description Remove addons buttons from Stremio
+// @icon        https://t3.gstatic.com/faviconV2?client=SOCIAL&url=http://stremio.com
+// ==/UserScript==
+(function () {
+  'use strict'
+  new MutationObserver(() => {
+    document.querySelectorAll('a[title="Addons"]').forEach(el => el.remove())
+    if (location.href.includes('/#/addons')) location.href = 'https://web.stremio.com/#/'
+  }).observe(document.body, { childList: true, subtree: true })
+})()
